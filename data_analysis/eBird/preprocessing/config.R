a6559e6bab0268809a8ed15df2b73f37e82a3298
Hamish Campbell <91635473+Hamish-Cam@users.noreply.github.com>
2022-05-16 12:41:33 +0100

    Updates config comments

--- data_analysis/eBird/preprocessing/config.R
+++ data_analysis/eBird/preprocessing/config.R
@@ -14,13 +14,13 @@ species_name_scientific <- "Coracina macei"
 # Continent of study (not strictly necessary but I am only looking at Asia atm)
 continent <- "Asia"
 
-# Name of the folder that contains input data folder
+# Name of the folder that contains input data folder for species specific data
 data_folder <- "data-lcs"
 
 # Name of downloaded eBird species data file
 species_data <- "ebd_larcus1_relApr-2022.txt"
 
-# Path to permanent location containing sampling and elevation data
+# Path to permanent location containing general data (sampling, elevation, range etc.)
 perm_files_location <- "~/hunting_data"
 
 # Name of downloaded sampling data file 
