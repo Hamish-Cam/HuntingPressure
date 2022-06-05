46da43729b20ac6e1352fe112a2073f16565be1d
Hamish Campbell <hrac2@cam.ac.uk>
2022-06-05 10:37:15 +0100

    Updates species list in abun config

--- data_analysis/eBird/abundance_mapping/config.R
+++ data_analysis/eBird/abundance_mapping/config.R
@@ -1,13 +1,40 @@
 # Config script for 'abundance_model_comparison.R'
 
 
+################ Species Sets ######################
+
+# Specify the sets of hunted and non-hunted species used for analysis
+# Entries must be in the form:
+#   c("common name" (according to eBird), "scientific name" (according to IUCN))
+hunted_species <- data.frame(c("Great Hornbill", "Buceros bicornis"),
+                            c("Oriental Pied-Hornbill", "Anthracoceros albirostris"),
+                            c("Wreathed Hornbill", "Rhyticeros undulatus"),
+                            c("Scaly-breasted Partridge", "Tropicoperdix chloropus"),
+                            c("Green Imperial-Pigeon", "Ducula aenea"),
+                            c("Ashy-headed Green-Pigeon", "Treron phayrei"),
+                            c("Alexandrine Parakeet", "Palaeornis eupatria"),
+                            c("Blossom-headed Parakeet", "Himalayapsitta roseata"),
+                            c("White-rumped Shama",	"Kittacincla malabarica"),
+                            c("Indian White-eye", "Zosterops palpebrosus"))
+
+non_hunted_species <- data.frame(c("Banded Bay Cuckoo", "Cacomantis sonneratii"),
+                                 c("Large Cuckooshrike", "Coracina javensis"),
+                                 c("Greater Racket-tailed Drongo", "Dicrurus paradiseus"),
+                                 c("Blyth's Frogmouth", "Batrachostomus javensis"),
+                                 c("Large-tailed Nightjar",	"Caprimulgus macrurus"),
+                                 c("Rufous Woodpecker",	"Micropternus brachyurus"),
+                                 c("Plain Flowerpecker", "Dicaeum minullum"),
+                                 c("Plain-backed Sparrow", "Passer flaveolus"),
+                                 c("Sooty-headed Bulbul",	"Pycnonotus aurigaster"),
+                                 c("Common Tailorbird",	"Orthotomus sutorius"))
+
+################ end ######################
+
+
 ################ Parameters ######################
 
-
-# Specify which species to study - Entries must be in the form:
-#     c(common name, scientific name used by IUCN)
-selected_species <- data.frame(c("Great Hornbill", "Buceros bicornis"),
-                               c("Large Cuckooshrike", "Coracina javensis"))
+# Specify whether to use the hunted or non-hunted species sets
+selected_species <- hunted_species
 
 # Selection of non-habitat variables to include in the advanced model 
 non_habitat_covariates <- c("hunting_mean")   
